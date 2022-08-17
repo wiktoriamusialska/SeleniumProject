0f815b47ce29452afb62996a69c4e582fa555e28
wiktoriamusialska <wiktoria.musialska@interia.pl>
2022-08-17 11:03:13 +0200

    -changes

--- src/sampleapp/WaitTest.java
+++ src/sampleapp/WaitTest.java
@@ -5,25 +5,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
 public class WaitTest {
     @Test
-    public void executeJavaScript() throws InterruptedException {
+    public void executeJavaScript()  {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\victoria\\Downloads\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://testeroprogramowania.github.io/selenium/wait2.html");
         driver.findElement(By.id("clickOnMe")).click();
 
-/*
-        //czekanie na element
-        Thread.sleep(5000);
-        driver.findElement(By.cssSelector("p"));
-*/
 
-        //drugi sposob na czekanie na elementy
+        //czekanie na element
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         driver.findElement(By.cssSelector("p"));
+
 
 
     }}
